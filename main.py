c21da2d223cca9eef4519e4f5230aea06f08c919
Marc Alberto <127353685+Marckus-programmer@users.noreply.github.com>
2023-03-12 15:58:35 +0000

    I change the main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #creem l'objecte de la classe importada, classe importada = Flask
 app = Flask('__name__')
@@ -6,7 +6,7 @@ app = Flask('__name__')
 #route= part del URL
 @app.route('/')
 def hello_world():
-  return 'hello, world'
+  return render_template('home.html')
 
 #if serveix per inicialitzar l'objecte app i obrir la pagina web
 if __name__ == '__main__':
